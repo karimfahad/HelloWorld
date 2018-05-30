720e455fbf742e346f834d7fa606c7325148d8c5
Fahad Karim <karimfahad@yahoo.com>
2018-05-29 20:00:25 -0400

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		int myFirstNumber = (5 + 10) * (4 + 6);
+		int mySecondNumber = 10;
+		int totalNumber = myFirstNumber + mySecondNumber;
+		int total1Number = 1000 - totalNumber;
+		System.out.println(total1Number);
+	}
+}
